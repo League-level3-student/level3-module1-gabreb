073fb732482e97332a498331b1509d26ec7fce11
gabreb <62906858+gabreb@users.noreply.github.com>
2020-06-26 15:08:24 -0700

    hi

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,29 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> d = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for (int i = 0; i < 100; i++) {
+			Double ray = new Random().nextDouble()*100;
+			d.push(ray);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		String num1 = JOptionPane.showInputDialog("Enter a number between 0 and 100 inclusive!");
+		String num2 = JOptionPane.showInputDialog("Enter another number between 0 and 100 inclusive!");
+		int num = Integer.parseInt(num1);
+		int numm = Integer.parseInt(num2);
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		int deeme = d.size();
+		for (int i = 0; i < deeme; i++) {
+			Double b = d.pop();
+			if (b >= num && b <= numm) {
+				System.out.println(b); 
+			}
+			else if (b <= num && b >= numm) {
+				System.out.println(b);
+			}		
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,6 +1,14 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener{
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,6 +21,41 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<String> stack = new Stack<String>();
 	
+	_02_TextUndoRedo() {
+		frame.add(panel);
+		panel.add(label);
+		frame.setSize(700,400);
+		label.setSize(350, 300);
+		label.setText("asdasdasdsdasdasdasd");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		_02_TextUndoRedo t = new _02_TextUndoRedo();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+	
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		
+	}
 	
 }
